42be720dbe065fb2208bff7e9d61f4ad1fc70d5e
김민주(C) <rlaalswn4879@naver.com>
2022-07-30 23:45:37 +0900

    google login

--- app/src/main/java/com/example/nulli/ui/SplashActivity.kt
+++ app/src/main/java/com/example/nulli/ui/SplashActivity.kt
@@ -5,14 +5,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nulli.MainActivity
+import com.example.nulli.R
 import com.example.nulli.databinding.ActivitySplashBinding
 import com.example.nulli.ui.auth.AuthMailActivity
 import com.example.nulli.ui.auth.JoinActivity
 import com.example.nulli.ui.auth.LoginActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.gun0912.tedpermission.PermissionListener
@@ -20,6 +31,9 @@ import com.gun0912.tedpermission.normal.TedPermission
 
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private val RC_SIGN_IN = 9001
+
 
     val binding : ActivitySplashBinding by lazy {
         ActivitySplashBinding.inflate(layoutInflater)
@@ -43,6 +57,7 @@ class SplashActivity : AppCompatActivity() {
                 } else {
                     binding.btnLogin.visibility = View.VISIBLE
                     binding.btnJoin.visibility = View.VISIBLE
+                    binding.btnGoogleSignIn.visibility = View.VISIBLE
                 }
 
             },2000)
@@ -61,14 +76,28 @@ class SplashActivity : AppCompatActivity() {
             .setPermissionListener(permissionlistener)
             .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
             .setPermissions(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.CAMERA,
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-        )
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            )
             .check();
 
+        auth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        var googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInGoogleBtn:SignInButton = findViewById(R.id.btn_googleSignIn)
+
+        signInGoogleBtn.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
 
 
         binding.btnLogin.setOnClickListener {
@@ -82,5 +111,34 @@ class SplashActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Toast.makeText(this,"로그인 실패", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Log.d("xxxx ", user.toString())
+                } else {
+                    Log.d("xxxx ", "signInWithCredential:failure", task.exception)
+                }
+
+            }
     }
 }
