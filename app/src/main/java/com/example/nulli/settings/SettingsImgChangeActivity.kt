d7817e11366c8a2757f613a9d4cd5befa88aac5d
sajieun <sajieun000721@naver.com>
2022-08-13 23:49:05 +0900

    탈퇴하기, 검색, 비밀번호변경, 프로필변경, 닉네임설정 xml 디자인 완료

---
+++ app/src/main/java/com/example/nulli/settings/SettingsImgChangeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nulli.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nulli.R
+
+class SettingsImgChangeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_img_change)
+    }
+}
---
+++ app/src/main/java/com/example/nulli/settings/SettingsWithdrawalActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nulli.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nulli.R
+
+class SettingsWithdrawalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_withdrawal)
+    }
+}
