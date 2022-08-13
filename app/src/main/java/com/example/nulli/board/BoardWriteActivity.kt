00e437341da54e4fac70498ecd6cc4c2ceafeb2a
김민정 <kmj127500@naver.com>
2022-08-14 05:31:51 +0900

    redesign

---
+++ app/src/main/java/com/example/nulli/board/BoardWriteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nulli.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nulli.R
+
+class BoardWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_write)
+    }
+}
