8bd5a05b4308fbb30ccff433e8d4797f0af38927
sajieun <sajieun000721@naver.com>
2022-08-17 03:07:34 +0900

    푸시 알림 설정 디자인 완료

---
+++ app/src/main/java/com/example/nulli/settings/SettingsNotificationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nulli.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsNotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_notification)
+    }
+}
