867c7b6f410348bf39bddc415a3b681d41020ed6
김민주(C) <rlaalswn4879@naver.com>
2022-08-17 10:26:56 +0900

    backbutton invisible

--- app/src/main/java/com/example/nulli/settings/SettingsNotificationActivity.kt
+++ app/src/main/java/com/example/nulli/settings/SettingsNotificationActivity.kt
@@ -2,6 +2,7 @@ package com.example.nulli.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nulli.R
 
 class SettingsNotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
