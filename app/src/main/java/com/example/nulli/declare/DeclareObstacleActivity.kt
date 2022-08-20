fee0298918f0bcf4e7a182be924e99ff7cdca9a1
김민정 <kmj127500@naver.com>
2022-08-21 03:47:51 +0900

    신고 기능(Settings-문의하기-declare.[Package])

---
+++ app/src/main/java/com/example/nulli/declare/DeclareObstacleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nulli.declare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nulli.R
+
+class DeclareObstacleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_declare_obstacle)
+    }
+}
---
+++ app/src/main/java/com/example/nulli/settings/SettingsDeclareActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nulli.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nulli.R
+
+class SettingsDeclareActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_declare)
+
+    }
+}
