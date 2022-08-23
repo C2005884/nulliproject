97b946c94fea9dc10375eb13d45cdc252817467f
김민주(C) <rlaalswn4879@naver.com>
2022-08-23 19:54:29 +0900

    비밀번호 변경 dialog

--- app/src/main/java/com/example/nulli/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/nulli/ui/mypage/MypageFragment.kt
@@ -1,6 +1,7 @@
 package com.example.nulli.ui.mypage
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,10 +9,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.nulli.databinding.FragmentMypageBinding
-import com.example.nulli.settings.*
+import com.example.nulli.settings.SettingsDeclareActivity
+import com.example.nulli.settings.SettingsImgChangeActivity
+import com.example.nulli.settings.SettingsNickChangeActivity
+import com.example.nulli.settings.SettingsWithdrawalActivity
 import com.example.nulli.ui.SplashActivity
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -92,7 +97,16 @@ class MypageFragment : Fragment() {
             val emailAddress = fuser?.email
            // val dialog = PwchangeConfirmDialog().apply {  }.dialog.show(supportFragmentManager, null)
 
-            Toast.makeText(requireContext(), "이메일이 전송되었습니다. 이메일을 확인해주세요", Toast.LENGTH_LONG).show()
+            val builder = AlertDialog.Builder(requireContext())
+            builder
+                .setTitle("비밀번호 변경")
+                .setMessage("이메일이 전송되었습니다. 이메일을 확인해주세요")
+                .setPositiveButton("확인",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // Start 버튼 선택시 수행
+                    })
+            builder.create()
+            builder.show()
 
             Firebase.auth.sendPasswordResetEmail(emailAddress!!)
                 .addOnCompleteListener { task ->
