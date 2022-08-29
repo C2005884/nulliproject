1bdb8f3ddb3e430f1e02825af60c95649e991e1c
sajieun <sajieun000721@naver.com>
2022-08-29 21:33:21 +0900

    최근검색어

--- app/src/main/java/com/example/nulli/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/nulli/ui/search/SearchFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.nulli.databinding.FragmentSearchBinding
+import com.example.nulli.util.SharedPreferencesManager
+import com.example.nulli.util.WrapContentLinearLayoutManager
 
 class SearchFragment : Fragment() {
-
+    private val pref:SharedPreferencesManager by lazy {
+        SharedPreferencesManager(requireContext())
+    }
     private var _binding: FragmentSearchBinding? = null
 
     // This property is only valid between onCreateView and
@@ -25,6 +30,23 @@ class SearchFragment : Fragment() {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRv()
+        binding.etSearch.setOnEditorActionListener { textView, i, keyEvent ->
+            pref.addSearchHistorySet(textView.text.toString())
+            textView.setText("")
+            Toast.makeText(requireContext(),pref.searchHistorySet.toString(),Toast.LENGTH_SHORT).show()
+            false
+        }
+    }
+
+    private fun setRv() {
+        binding.rvSearch.apply {
+            layoutManager = WrapContentLinearLayoutManager(requireContext())
+        }
     }
 
     override fun onDestroyView() {
